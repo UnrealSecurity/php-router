c1e31d83f7558478fce6adbdf7ef01909dbfdb8b
UnrealSec <arran.bishop89@aol.com>
2020-11-14 17:07:03 +0200

    Update Router.php

--- Router.php
+++ Router.php
@@ -227,11 +227,81 @@
             $includes = ['php', 'html'];
             $dirless_exts = ['php', 'html'];
             $media = ['mp4', 'mp3', 'ogg', 'webm', 'avi', 'wav', 'mov', 'mpeg', 'mpg'];
-            $mimetypes = [
-                'js' => 'text/javascript',
-                'html' => 'text/html',
-                'php' => 'text/html',
-            ];
+			$mimetypes = [
+				'htm' => 'text/html',
+				'html' => 'text/html',
+				'js' => 'text/javascript',
+				'txt' => 'text/plain',
+				'bmp' => 'image/bmp',
+				'jpg' => 'image/jpeg',
+				'jpeg' => 'image/jpeg',
+				'png' => 'image/png',
+				'css' => 'text/css',
+				'aac' => 'audio/aac',
+				'abw' => 'application/x-abiword',
+				'arc' => 'application/x-freearc',
+				'avi' => 'video/x-msvideo',
+				'azw' => 'application/vnd.amazon.ebook',
+				'bin' => 'application/octet-stream',
+				'bz' => 'application/x-bzip',
+				'bz2' => 'application/x-bzip2',
+				'csh' => 'application/x-csh',
+				'csv' => 'text/csv',
+				'doc' => 'application/msword',
+				'docx' => 'application/vnd.openxmlformats-',
+				'eot' => 'application/vnd.ms-fontobject',
+				'epub' => 'application/epub+zip',
+				'gz' => 'application/gzip',
+				'gif' => 'image/gif',
+				'ico' => 'image/x-icon',
+				'ics' => 'text/calendar',
+				'jar' => 'application/java-archive',
+				'json' => 'application/json',
+				'jsonld' => 'application/ld+json',
+				'mid' => 'audio/midi',
+				'midi' => 'audio/midi',
+				'mjs' => 'text/javascript',
+				'mp3' => 'audio/mpeg',
+				'mp4' => 'video/mp4',
+				'mpeg' => 'video/mpeg',
+				'mpkg' => 'application/vnd.apple.installer+xml',
+				'odp' => 'application/vnd.oasis.opendocument.presentation',
+				'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+				'odt' => 'application/vnd.oasis.opendocument.text',
+				'oga' => 'audio/ogg',
+				'ogv' => 'video/ogg',
+				'ogx' => 'application/ogg',
+				'opus' => 'audio/opus',
+				'otf' => 'font/otf',
+				'pdf' => 'application/pdf',
+				'php' => 'text/html',
+				'ppt' => 'application/vnd.ms-powerpoint',
+				'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+				'rar' => 'application/vnd.rar',
+				'rtf' => 'application/rtf',
+				'sh' => 'application/x-sh',
+				'svg' => 'image/svg+xml',
+				'swf' => 'application/x-shockwave-flash',
+				'tar' => 'application/x-tar',
+				'tif' => 'image/tiff',
+				'tiff' => 'image/tiff',
+				'ts' => 'video/mp2t',
+				'ttf' => 'font/ttf',
+				'vsd' => 'application/vnd.visio',
+				'wav' => 'audio/wav',
+				'weba' => 'audio/webm',
+				'webm' => 'video/webm',
+				'webp' => 'image/webp',
+				'woff' => 'font/woff',
+				'woff2' => 'font/woff2',
+				'xhtml' => 'application/xhtml+xml',
+				'xls' => 'application/vnd.ms-excel',
+				'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+				'xml' => 'application/xml',
+				'xul' => 'application/vnd.mozilla.xul+xml',
+				'zip' => 'application/zip',
+				'7z' => 'application/x-7z-compressed'
+			];
 
             if ($self == null || $values == null) {
                 echo '<h2>Error!</h2> Call to <b>Router::serve()</b> failed because <b>$self</b> and/or <b>$values</b> was not set'; 
@@ -256,7 +326,7 @@
                             $parts = explode('.', $root.$path.'/'.$index);
                             $ext = (count($parts) > 1 ? strtolower($parts[count($parts)-1]) : null);
 
-                            if ($ext != null && array_key_exists('js', $mimetypes)) {
+                            if ($ext != null && array_key_exists($ext, $mimetypes)) {
                                 header('Content-Type: '.$mimetypes[$ext]);
                             }
 
